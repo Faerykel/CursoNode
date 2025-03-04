dade9e6fe3a4a17b46c2500745c8e70b37bb84a7
Raquel <raquel.rlima97@gmail.com>
2025-03-03 21:04:00 -0300

    Teste de commit

---
+++ teste.js
@@ -0,0 +1,16 @@
+const crypto = require("crypto");
+const start = Date.now();
+
+function logHash.time(){
+    crypto.pbkdf2("a","b", 100000, 512, sha512, () =>{
+        console..log(`Hash: ${Date.now() - start}`);
+    })
+}
+
+logHash.time();
+
+logHash.time();
+
+logHash.time();
+
+logHash.time();
